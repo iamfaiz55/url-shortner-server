f40e05b4006f09fdf49d2d8d66a1762383bc98a2
john <you@example.com>
2024-07-10 13:20:12 +0530

    url update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://shortly-iemi.onrender.com",
+    origin: "*",
     credentials: true
 }))
 
